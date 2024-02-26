c5a33cf8238c537378ba816dfbc524b5f491a240
Mardaaa <martin_tp@live.dk>
2024-02-26 09:49:09 +0100

    added sql exercises

---
+++ Kursusgang 2/SQL_script.py
@@ -0,0 +1,27 @@
+import sqlite3
+from sqlite3 import Error
+
+# Opret connection
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+# Create database file
+connection = create_connection("school.db")
+
+# Function to execute queries
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
