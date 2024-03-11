f5fac86a7186e7678d6a36a23c04cd2d5923eec5
Mardaaa <martin_tp@live.dk>
2024-03-11 12:49:18 +0100

    added loading function

--- Kursusgang 4/exercise_2.py
+++ Kursusgang 4/exercise_2.py
@@ -2,8 +2,30 @@ import os
 import pandas as pd
 
 
-path = os.path.join(os.path.dirname(__name__), 'recipeData.csv')
+def load_data(file_name):
+    try:
+        path = os.path.join(os.path.dirname(__file__), f'{file_name}')
+    except Exception as e:
+        print(f"There has occurred an error: {e}")
+    else:
+        df = pd.read_csv(path, encoding='ISO-8859-1').set_index('BeerID')
+    return df
 
-df = pd.read_csv(path)
 
-print(df.head())
+    
+
+
+def main():
+    df = load_data('recipeData.csv')
+    
+    ##### Drop URL column #####
+    df.drop('URL',axis=1, inplace=True)
+    print(df.head())
+
+
+    ##### Find missing values #####
+    missing_values = df.isnull().sum()
+    print(missing_values)
+
+if __name__ == '__main__':
+    main()
