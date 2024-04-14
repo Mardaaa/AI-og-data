2f9441410e02c4886d82d1c1e52a3fe821bf3437
Mardaaa <martin_tp@live.dk>
2024-04-14 19:24:12 +0200

    removed ret

--- Kursusgang 1/image_files.py
+++ Kursusgang 1/image_files.py
@@ -5,12 +5,12 @@ def take_picture():
     cap = cv2.VideoCapture(0) # 0 is webcam index
     
     while(True):
-        ret, frame = cap.read() # Capture frame-by-frame
+        _, frame = cap.read() # Capture frame-by-frame
         cv2.imshow('img1', frame)
         key = cv2.waitKey(1)
         if key == ord('y'): # Press 'y' to take a picture
             current_dir = os.path.dirname(os.path.abspath(__file__))
-            image_path = os.path.join(current_dir, 'image1.png')
+            image_path = os.path.join(current_dir, 'image2.png')
             cv2.imwrite(image_path, frame)
             print("Image captured!")
             break
@@ -32,5 +32,5 @@ def load_picture(file_name):
         cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    # take_picture()
-    load_picture('image1.png')
+    take_picture()
+    # load_picture('image1.png')
